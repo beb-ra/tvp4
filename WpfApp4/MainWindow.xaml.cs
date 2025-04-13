04fc219414403feac42c6161223b1f0d4b118003
bebra <gusol2468@gmail.com>
2025-04-13 16:05:50 +0300

    bebebe

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    public partial class MainWindow : Window
+    {
+        Model model;
+        private Ellipse[,] ellipses;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            model = new Model(10, 10);
+            ellipses = new Ellipse[10, 10];
+            CreateGameBoard();
+        }
+
+        private void CreateGameBoard()
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    model.Init();
+
+                    Ellipse ellipse = new Ellipse();
+                    if (row == 0 && col == 0) 
+                    { 
+                        ellipse.Fill = Brushes.Red; 
+                    }
+                    else
+                    {
+                        if (row == 9 && col == 9)
+                        {
+                            ellipse.Fill = Brushes.Blue;
+                        }
+                        else
+                        {
+                            ellipse.Fill = Brushes.White;
+                        }
+                    }
+                    ellipse.MouseLeftButtonDown += Cell_MouseLeftButtonDown;
+
+                    ellipse.Width = 50;
+                    ellipse.Height = 50;
+
+                    Grid.SetRow(ellipse, row);
+                    Grid.SetColumn(ellipse, col);
+
+                    grid.Children.Add(ellipse);
+                    ellipses[row, col] = ellipse;
+                }
+            }
+        }
+
+        private void Cell_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse ellipse = (Ellipse)sender;
+            int row = Grid.GetRow(ellipse);
+            int col = Grid.GetColumn(ellipse);
+
+            if (model.CanMove(row, col, model.whoWalks))
+            {
+                AddVirus(row, col);
+            }
+
+            if (model.IsFinall())
+            {
+                this.Close();
+            }
+        }
+
+        private void AddVirus(int row, int col)
+        {
+            if (ellipses[row, col] != null)
+            {
+                model.AddObject(row, col);
+
+                if (model.field[row, col] == 1) 
+                {
+                    ellipses[row, col].Fill = Brushes.Red;
+                }
+                if (model.field[row, col] == 2)
+                {
+                    ellipses[row, col].Fill = Brushes.Blue;
+                }
+
+                if (model.field[row, col] == 3)
+                {
+                    ellipses[row, col].Fill = Brushes.Red;
+                    ellipses[row, col].Width = 100;
+                    ellipses[row, col].Height = 100;
+                    ellipses[row, col].HorizontalAlignment = HorizontalAlignment.Center;
+                    ellipses[row, col].VerticalAlignment = VerticalAlignment.Center;   
+                }
+                if (model.field[row, col] == 4)
+                {
+                    ellipses[row, col].Fill = Brushes.Blue;
+                    ellipses[row, col].Width = 100;
+                    ellipses[row, col].Height = 100;
+                    ellipses[row, col].HorizontalAlignment = HorizontalAlignment.Center;
+                    ellipses[row, col].VerticalAlignment = VerticalAlignment.Center;
+                }
+            }
+        }
+
+    }
+}
---
+++ WpfApp4/Model.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp4
+{
+    internal class Model
+    {
+        public int[,] field;
+        int _rows, _cols;
+        public int whoWalks = 1, countMovesDone = 0;
+
+        public Model(int rows, int cols) 
+        { 
+            _rows = rows;
+            _cols = cols;
+            field = new int[rows, cols];
+        }
+
+        internal void AddObject(int row, int col)
+        {
+            if (field[row, col] == 0)
+            {
+                field[row, col] = whoWalks;
+            }
+            if (field[row, col] == 2 && whoWalks == 1 || field[row, col] == 1 && whoWalks == 2)
+            {
+                field[row, col] = whoWalks + 2;
+            }
+
+            /*
+            string message = "";
+
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _cols; j++)
+                {
+                    message += field[i, j].ToString() + " "; 
+                }
+                message += "\n"; 
+            }
+
+            MessageBox.Show(message, "Содержимое массива");
+            */
+            MoveDone();
+        }
+
+        internal bool CanMove(int row, int col, int who)
+        {
+            if (field[row, col] == 0 || (field[row, col] == 2 && who == 1 || field[row, col] == 1 && who == 2))
+            {
+                if (row > 0)
+                {
+                    if (field[row - 1, col] == who || (field[row - 1, col] == who + 2 && IsAliveAround(row-1, col, who)))
+                    {
+                        return true;
+                    }
+                    if (col > 0)
+                    {
+                        if (field[row - 1, col - 1] == who || (field[row - 1, col - 1] == who + 2 && IsAliveAround(row-1, col-1, who)))
+                        {
+                            return true;
+                        }
+                    }
+                    if (col < _cols - 1)
+                    {
+                        if (field[row - 1, col + 1] == who || (field[row - 1, col + 1] == who + 2 && IsAliveAround(row-1, col+1, who)))
+                        {
+                            return true;
+                        }
+                    } 
+                }
+                if (col > 0)
+                {
+                    if (field[row, col - 1] == who || (field[row, col - 1] == who + 2 && IsAliveAround(row,col-1,who)))
+                    {
+                        return true;
+                    }
+                    if (row < _rows - 1)
+                    {
+                        if (field[row + 1, col - 1] == who || (field[row + 1, col - 1] == who + 2 && IsAliveAround(row + 1, col - 1, who)))
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                if (row < _rows - 1)
+                {
+                    if (field[row + 1, col] == who || (field[row + 1, col] == who + 2 && IsAliveAround(row + 1, col, who)))
+                    {
+                        return true;
+                    }
+                    if (col < _cols - 1)
+                    {
+                        if (field[row + 1, col + 1] == who || (field[row + 1, col + 1] == who + 2 && IsAliveAround(row + 1, col + 1, who)))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                if (col < _cols - 1)
+                {
+                    if (field[row, col + 1] == who || (field[row, col + 1] == who + 2 && IsAliveAround(row, col + 1, who)))
+                    {
+                        return true;
+                    }
+                }
+            } 
+
+            return false;
+        }
+
+        private bool IsAliveAround(int row, int col, int who)
+        {
+            if (row > 0)
+            {
+                if (field[row - 1, col] == who)
+                {
+                    return true;
+                }
+                if (col > 0)
+                {
+                    if (field[row - 1, col - 1] == who)
+                    {
+                        return true;
+                    }
+                }
+                if (col < _cols - 1)
+                {
+                    if (field[row - 1, col + 1] == who)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (col > 0)
+            {
+                if (field[row, col - 1] == who)
+                {
+                    return true;
+                }
+                if (row < _rows - 1)
+                {
+                    if (field[row + 1, col - 1] == who)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if (row < _rows - 1)
+            {
+                if (field[row + 1, col] == who)
+                {
+                    return true;
+                }
+                if (col < _cols - 1)
+                {
+                    if (field[row + 1, col + 1] == who)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (col < _cols - 1)
+            {
+                if (field[row, col + 1] == who)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+    }
+
+        internal void Init()
+        {
+            field[0, 0] = 1; field[_rows - 1, _cols - 1] = 2;
+        }
+
+        internal bool IsFinall()
+        {
+            bool isWinRed = true, isWinBlue = true;
+            for (int i = 0; i < _rows; i++)
+            {
+                for (int j = 0; j < _cols; j++)
+                {
+                    if (field[i, j] == 1)
+                    {
+                        isWinBlue = false;
+                    }
+                    if (field[i, j] == 2)
+                    {
+                        isWinRed = false;
+                    }
+
+                    if (!isWinRed && !isWinBlue) break;
+                }
+            }
+
+
+            if (isWinRed && !isWinBlue)
+            {
+                MessageBox.Show("Красные выиграли");
+            }
+            if (isWinBlue && !isWinRed)
+            {
+                MessageBox.Show("Красные выиграли");
+            }
+            if (isWinBlue && isWinRed)
+            {
+                MessageBox.Show("Ничья");
+            }
+            return isWinRed || isWinBlue;
+        }
+
+        internal void MoveDone()
+        {
+            countMovesDone++;
+            if (countMovesDone == 3) 
+            { 
+                countMovesDone = 0;
+
+                if (whoWalks == 1)
+                {
+                    whoWalks = 2;
+                }
+                else
+                {
+                    whoWalks = 1;
+                }
+            }
+        }
+    }
+}
